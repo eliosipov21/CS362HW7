2126edea178a344b5a43000c9eb53dc074ccc09e
eliosipov21 <eli.osipov2@gmail.com>
2021-05-31 22:17:34 -0700

    Wrote second test case

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,6 +5,9 @@ class testCaseAdd(unittest.TestCase):
     def test_add(self):
         result = fizzbuzz.fbuzz(15)
         self.assertEqual(result, "Fizzbuzz")
+    def test_add(self):
+        result = fizzbuzz.fbuzz(3)
+        self.assertEqual(result, "Fizz")
     
 
 if __name__ == '__main__':
