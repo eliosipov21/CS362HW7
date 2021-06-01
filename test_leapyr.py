5180d4e5147199d507bfe021da0fb7a1d1480943
eliosipov21 <eli.osipov2@gmail.com>
2021-05-31 22:55:28 -0700

    Wrote third testcase for leapyear

--- test_leapyr.py
+++ test_leapyr.py
@@ -5,8 +5,11 @@ class testCaseAdd(unittest.TestCase):
     def test_leap1(self):
         result = leapyr.leapyear(404)
         self.assertEqual(result, True)
-    def test_leap1(self):
+    def test_leap2(self):
         result = leapyr.leapyear(500)
+        self.assertEqual(result, False)
+    def test_leap3(self):
+        result = leapyr.leapyear(400)
         self.assertEqual(result, False)
 
     
