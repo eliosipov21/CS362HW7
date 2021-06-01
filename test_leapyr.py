abb53055bfc4d71f548baadcf37b85c2666a385c
eliosipov21 <eli.osipov2@gmail.com>
2021-05-31 22:53:15 -0700

    Wrote code for second leapyear testcase

--- test_leapyr.py
+++ test_leapyr.py
@@ -5,6 +5,10 @@ class testCaseAdd(unittest.TestCase):
     def test_leap1(self):
         result = leapyr.leapyear(404)
         self.assertEqual(result, True)
+    def test_leap1(self):
+        result = leapyr.leapyear(500)
+        self.assertEqual(result, False)
+
     
 
 if __name__ == '__main__':
